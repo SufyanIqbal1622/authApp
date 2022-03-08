841024fbcf69e9be8fe36b7cc1012c667ff54afa
Sufyan Iqbal <Sufyan.Iqbal@activekeysolutions.com>
2022-03-08 16:08:14 +0500

    user roles added

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,10 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[7.0].define(version: 2022_03_07_162556) do
+ActiveRecord::Schema[7.0].define(version: 2022_03_08_091950) do
+  # These are extensions that must be enabled in order to support this database
+  enable_extension "plpgsql"
+
   create_table "users", force: :cascade do |t|
     t.string "email", default: "", null: false
     t.string "encrypted_password", default: "", null: false
@@ -19,6 +22,7 @@ ActiveRecord::Schema[7.0].define(version: 2022_03_07_162556) do
     t.datetime "remember_created_at"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
+    t.integer "role", default: 0
     t.index ["email"], name: "index_users_on_email", unique: true
     t.index ["reset_password_token"], name: "index_users_on_reset_password_token", unique: true
   end
